759cccecfb2e4f9e0713df8882df0279f1a6c3b0
shengjiyang <samswank@gmail.com>
2020-08-06 14:09:19 -0600

    mvp for markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,4 +1,3 @@
-import numpy as np
 import random
 
 # Read in all the words in one go
@@ -17,4 +16,54 @@ for first, second in pairs:
 
     d[first].append(second)
 
-print(d)
+
+start = []
+for w in list(d.keys()):
+    if w[0] == w[0].upper() and w[0] != "(" and w[0:2] != '"-' and w[-1] != ")" and w[-1] != "." and w[-1] != "!":
+        start.append(w)
+
+stop = []
+for w in list(d.keys()):
+    if w[-1] == "." or w[-1] == "!" or w[-1] == "?":
+        stop.append(w)
+
+random.seed(84)
+print(random.choice(start), end=" ")
+print(random.choice(d["As"]), end=" ")
+print(random.choice(d[random.choice(d["As"])]), end=" ")
+print(random.choice(d[random.choice(d[random.choice(d["As"])])]), end=" ")
+print(random.choice(d["if"]), end=" ")
+print(random.choice(stop))
+
+# Sentence 1: "As soon as if I declare!"
+
+print(random.choice(start), end=" ")
+print(random.choice(d["It"]), end=" ")
+print(random.choice(d[random.choice(d["It"])]), end=" ")
+print(random.choice(stop))
+
+# Sentence 2: "It was the pencil."
+
+print(random.choice(start), end=" ")
+print(random.choice(d["How"]), end=" ")
+print(random.choice(d[random.choice(d["How"])]), end=" ")
+print(random.choice(d[random.choice(d[random.choice(d["How"])])]), end=" ")
+print(random.choice(d[random.choice(d[random.choice(d[random.choice(d["How"])])])]), end=" ")
+print(random.choice(stop))
+
+# Sentence 3: "How would check, see get manners!"
+
+print(random.choice(start), end=" ")
+print(random.choice(stop))
+
+# Sentence 4: "I've manners!"
+
+print(random.choice(start), end=" ")
+print(random.choice(d[random.choice(start)]), end=" ")
+print(random.choice(d[random.choice(d[random.choice(start)])]), end=" ")
+print(random.choice(d[random.choice(d[random.choice(d[random.choice(start)])])]), end=" ")
+print(random.choice(d["all"]), end=" ")
+print(random.choice(d[random.choice(d["all"])]), end=" ")
+print(random.choice(stop))
+
+# Sentence 5: "Here the some all the outside. Kitty?"
