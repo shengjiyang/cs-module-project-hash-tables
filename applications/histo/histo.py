224b5b6c11eb4f6c79a598f32f3474b23f5fb450
shengjiyang <samswank@gmail.com>
2020-08-06 14:37:57 -0600

    histogram

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,29 @@
-# Your code here
+# applications/histo/histo.py
 
+URL = "/Users/samuel/Programming/GitHub/Lambda School/Assignments/cs-module-project-hash-tables/applications/histo/robin.txt"
+with open(URL) as f:
+    words = f.read()
+
+def word_count(s):
+    d = {}
+    words = []
+    for w in s.split():
+        w = w.strip(",").strip(".").strip('"')
+        words.append(w.lower())
+
+    for w in words:
+        if ":;,.-+=/\\|[]{}()*^&" in words:
+            return {}
+
+        else:
+            d[w] = words.count(w)
+
+    return d
+
+def histo(s):
+    d = word_count(s)
+    table = sorted(list(d.items()), key=lambda i: (-i[1], i[0]))
+    for word, freq in table:
+        print(f"{word} " + "#" * freq)
+
+histo(words)
